c56eb10b176c1fdf5aca0e33889a0e8e5418cc21
dclinton <dclinton@localhost>
2009-03-26 03:12:40 +0000

    Explicitly expose the .message accessor on TwitterError instances

--- twitter.py
+++ twitter.py
@@ -40,6 +40,11 @@ CHARACTER_LIMIT = 140
 
 class TwitterError(Exception):
   '''Base class for Twitter errors'''
+  
+  @property
+  def message(self):
+    '''Returns the first argument used to construct this error.'''
+    return self.args[0]
 
 
 class Status(object):

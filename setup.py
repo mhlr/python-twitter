df52195a5898cbe38586b3b188e5e0e2bd7f89a7
Mike Taylor <bear42@gmail.com>
2011-04-16 17:48:28 -0400

    Issue 181 http://code.google.com/p/python-twitter/issues/detail?id=181 Add oauth2 to install_requires parameter list and also updated README to note that the oauth2 lib can be found in two locations

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ METADATA = dict(
 
 # Extra package metadata to be used only if setuptools is installed
 SETUPTOOLS_METADATA = dict(
-  install_requires = ['setuptools', 'simplejson'],
+  install_requires = ['setuptools', 'simplejson', 'oauth2'],
   include_package_data = True,
   classifiers = [
     'Development Status :: 4 - Beta',
